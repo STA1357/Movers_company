41be936773b1edcb550f025db9ed90bdac0a38bd
NussArtem <artem_nuss@mail.ru>
2021-01-05 19:14:46 +0200

    add more features))

--- src/components/UIComponents/Button.vue
+++ src/components/UIComponents/Button.vue
@@ -1,37 +1,36 @@
 <template>
-    <span>
-        {{text}}
-    </span>
+  <span>
+    {{ text }}
+  </span>
 </template>
 
 <script>
-  //  @params text(String)
-    export default {
-        name: "Button",
-        props:{
-            text: String
-        }
-    }
+//  @params text(String)
+export default {
+  name: "Button",
+  props: {
+    text: String
+  }
+};
 </script>
 
 <style scoped>
-    span{
-        background: #0F2F31;
-        border-radius: 11px;
-        width: 286px;
-        height: 52px;
+span {
+  background: #0f2f31;
+  border-radius: 11px;
+  width: 286px;
+  height: 52px;
 
-        @font-family: Montserrat;
-        font-style: normal;
-        font-weight: 500;
-        font-size: 18px;
-        line-height: 22px;
-        /* identical to box height */
+  @font-family: Montserrat;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 22px;
+  /* identical to box height */
 
-        text-align: center;
-        text-transform: uppercase;
-        color: #FFFFFF;
-        padding: 15px 6px;
-
-    }
+  text-align: center;
+  text-transform: uppercase;
+  color: #ffffff;
+  padding: 15px 6px;
+}
 </style>
--- src/components/UIComponents/Mark.vue
+++ src/components/UIComponents/Mark.vue
@@ -1,38 +1,37 @@
 <template>
-    <span class="mark pl-2">
-        {{text}}
-    </span>
+  <span class="mark pl-2">
+    {{ text }}
+  </span>
 </template>
 
 <script>
-    //  @params text(String)
+//  @params text(String)
 
-    export default {
-        name: "Mark",
-        props:{
-            text:String
-        }
-    }
+export default {
+  name: "Mark",
+  props: {
+    text: String
+  }
+};
 </script>
 
 <style scoped>
-    .mark {
-        width: 32px;
-        height: 15px;
-        background: #F2F2F2;
-        border-radius: 4px;
-        padding: 2px 5px;
-        text-transform: uppercase;
-        @font-family: Montserrat;
-        font-style: normal;
-        font-weight: 300;
-        font-size: 9px;
-        line-height: 11px;
-        /* identical to box height */
+.mark {
+  width: 32px;
+  height: 15px;
+  background: #f2f2f2;
+  border-radius: 4px;
+  padding: 2px 5px;
+  text-transform: uppercase;
+  @font-family: Montserrat;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 9px;
+  line-height: 11px;
+  /* identical to box height */
 
-        position: relative;
-        top: -2px;
-        color: #0F2F31;
-
-    }
+  position: relative;
+  top: -2px;
+  color: #0f2f31;
+}
 </style>
