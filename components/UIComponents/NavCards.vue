cfb2826a401346a5ad06bd2f6f45cbd44f88a691
NussArtem <artem_nuss@mail.ru>
2021-01-11 18:43:18 +0200

    update

--- components/UIComponents/NavCards.vue
+++ components/UIComponents/NavCards.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="nav mb-3">
-    <span class="col" v-for="(info, idx) in text" :key="idx">{{ info }}</span>
+    <router-link
+      :to="info.path"
+      class="col links"
+      v-for="(info, idx) in text"
+      :key="idx"
+      >{{ info.title }}</router-link
+    >
   </div>
 </template>
 
@@ -8,7 +14,10 @@
 export default {
   name: "NavCards",
   props: {
-    text: Array
+    text: {
+      title: String,
+      path: String
+    }
   }
 };
 </script>
@@ -20,38 +29,38 @@ export default {
   width: 314px;
   height: 29px;
   margin: 0 auto;
-}
-.nav span {
-  background: #f9f9f9;
-  text-transform: uppercase;
-  font-style: normal;
-  font-weight: 300;
-  font-size: 12px;
-  line-height: 15px;
-  /* identical to box height */
-
-  text-align: center;
-  border: 0.5px solid #0f2f31;
-
-  color: #0f2f31;
-  padding: 7px 12px;
-  &:last-child {
-    border-radius: 0px 20px 20px 0px;
-  }
-  &:first-child {
-    border-radius: 20px 0px 0px 20px;
-  }
-  &:hover {
-    background: #0f2f31;
+  .links {
+    background: #f9f9f9;
+    text-transform: uppercase;
     font-style: normal;
-    font-weight: bold;
+    font-weight: 300;
     font-size: 12px;
     line-height: 15px;
     /* identical to box height */
 
     text-align: center;
+    border: 0.5px solid #0f2f31;
 
-    color: #ffffff;
+    color: #0f2f31;
+    padding: 7px 12px;
+    &:last-child {
+      border-radius: 0px 20px 20px 0px;
+    }
+    &:first-child {
+      border-radius: 20px 0px 0px 20px;
+    }
+    &:hover {
+      background: #0f2f31;
+      font-style: normal;
+      font-weight: bold;
+      font-size: 12px;
+      line-height: 15px;
+      /* identical to box height */
+
+      text-align: center;
+
+      color: #ffffff;
+    }
   }
 }
 </style>
---
+++ pages/earn/basic.vue
@@ -0,0 +1,144 @@
+<template
+  ><div>
+    <div class="card">
+      <div class="block mb-2">
+        <t-block text="You give:" :text2="account.balance" />
+        <div class="d-flex justify-content-between mt-2">
+          <span class="col-3 txt">
+            <input v-model="eth" type="text" class="inputs" placeholder="0.0" />
+          </span>
+          <span class="col-6 pr-0">
+            <Mark :text="msg" @click.native="eth = account.balance" />
+            <span class="ml-3 txt">
+              <img src="@/assets/images/eth.svg" alt="" />
+              <span class="count">ETH</span>
+            </span>
+          </span>
+        </div>
+      </div>
+      <div class="p-2 ml-3">
+        <img src="@/assets/images/to.svg" alt="" style="float: left" />
+      </div>
+      <div class="block mb-2">
+        <t-block text="You give:" text2="0  " />
+        <div class="d-flex justify-content-between mt-2">
+          <span class="col-3 txt">
+            <input
+              v-model="measurementValueDisplay"
+              type="text"
+              class="inputs"
+              placeholder="0.0"
+              disabled
+            />
+          </span>
+          <span class="col-6 pr-0">
+            <span class="ml-3 txt">
+              <img src="@/assets/images/white.svg" alt="" />
+              <span class="count">WHITE</span>
+            </span>
+          </span>
+        </div>
+      </div>
+      <div class="block mb-2">
+        <t-block text="You give:" text2="0" />
+        <div class="d-flex justify-content-between mt-2">
+          <span class="col-3 txt">
+            <input
+              v-model="measurementValueDisplay"
+              disabled
+              type="text"
+              class="inputs"
+              placeholder="0.0"
+            />
+          </span>
+          <span class="col-6 pr-0">
+            <span class="ml-3 txt">
+              <img src="@/assets/images/black.svg" alt="" />
+              <span class="count">BLACK</span>
+            </span>
+          </span>
+        </div>
+      </div>
+      <div class="d-flex check-price justify-content-between">
+        <span class="col-5">Aggregate price:</span>
+        <span class="col-6">651.66 B&W per 1 ETH</span>
+      </div>
+
+      <Button text="RETURN BLACK & WHITE" @click.native="shotList" />
+    </div>
+    <list
+      :text-l="[
+        'WHITE Price',
+        'BLACK Price',
+        'ETH in Base Pool',
+        'WHITE in the market',
+        'BLACK in the market',
+        'WHITE in Base Pool',
+        'BLACK in Base Pool',
+        'Aggregate B&W price',
+        'Change B&W Price (1,7,30 days)'
+      ]"
+      :text-r="[
+        '0.00153454 ETH',
+        '0.00153454 ETH',
+        '1544 ETH',
+        '503082.355 WHITE',
+        '503082.355 BLACK',
+        '799496918 WHITE',
+        '799496918 BLACK',
+        '0,00306908 ETH',
+        '0.1/0.5/1.7 %'
+      ]"
+      :show="this.show"
+    />
+  </div>
+</template>
+
+<script>
+import TBlock from "@/components/UIComponents/TitleBlock";
+import Button from "@/components/UIComponents/Button";
+import Mark from "@/components/UIComponents/Mark";
+import List from "@/components/UIComponents/List";
+export default {
+  name: "basic",
+  components: {
+    Button,
+    Mark,
+    TBlock,
+    List
+  },
+  data() {
+    return {
+      msg: "MAX",
+      show: false,
+      eth: "",
+      whiteBlack: ""
+    };
+  },
+  computed: {
+    account() {
+      return this.$store.getters["web3/account"];
+    },
+    measurementValueDisplay: {
+      get() {
+        this.whiteBlack = this.eth * 651.66;
+        return this.whiteBlack;
+      },
+      set(newValue) {
+        return this.eth;
+      }
+    }
+  },
+  mounted() {
+    this.$store.dispatch("web3/getAccount");
+  },
+  methods: {
+    shotList() {
+      this.show = !this.show;
+      console.log(this.show);
+    }
+  }
+};
+</script>
+
+<style scoped></style>
